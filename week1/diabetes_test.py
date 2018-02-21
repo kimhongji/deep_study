45e30a3903db61355beec209d7cdba7b1125d0a0
dhd96 <dhd96@naver.com>
2018-02-21 16:36:38 +0900

    multi 로 수정

--- week1/diabetes_test.py
+++ week1/diabetes_test.py
@@ -14,17 +14,18 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 #데이터 갸져오기 
 diabetes = datasets.load_diabetes()
-
+diabetes_X =  diabetes.data
+diabetes_Y =  diabetes.target
 #diabetes 의 총 10개의 데이터중 2번째 데이터만 가져옴
-diabetes_X = diabetes.data[:, np.newaxis, 2]
+#diabetes_X = diabetes.data[:, np.newaxis, 2]
 
 #뒤에서 -20 까지의 데이터 를 train 
 #뒤에서 20 개의 데이터를 test 해서 학습 and 검증 
 diabetes_X_train = diabetes_X[:-20]
 diabetes_X_test = diabetes_X[-20:]
 
-diabetes_Y_train = diabetes.target[:-20]
-diabetes_Y_test = diabetes.target[-20:]
+diabetes_Y_train = diabetes_Y[:-20]
+diabetes_Y_test = diabetes_Y[-20:]
 
 #linear regression API 이용 
 regr = linear_model.LinearRegression()
@@ -41,9 +42,15 @@ print("Mean squard error : %.2f" % mean_squared_error(diabetes_Y_test, diabetes_
 
 print('Variance score : %.2f' % r2_score(diabetes_Y_test, diabetes_Y_pred))
 
+
+plt.plot(diabetes_Y_test)
+plt.plot(diabetes_Y_pred)
+plt.show()
+'''
 plt.scatter(diabetes_X_test,diabetes_Y_test, color='black' )
 plt.plot(diabetes_X_test,diabetes_Y_pred, color = 'blue',linewidth=3)
 
 plt.xticks()
 plt.yticks()
 plt.show()
+'''
